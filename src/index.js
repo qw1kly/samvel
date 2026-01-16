42aa84fe6cb2ef3fada19b0fc5b1167559864663
qw1kly <168624106+qw1kly@users.noreply.github.com>
2026-01-16 19:18:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { findAllByAltText } from '@testing-library/dom';
 
+document.body.style.overflow = "auto";
+document.body.style.overflowY = "auto";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
