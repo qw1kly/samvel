91f9326f601297f6c2e6f92842cf1f23e7d27eea
qw1kly <168624106+qw1kly@users.noreply.github.com>
2026-01-17 00:02:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ import { findAllByAltText } from '@testing-library/dom';
 setTimeout(() => {
 document.body.style.overflow = "auto";
 document.body.style.overflowY = "auto";
+}, 2000);
+
+setTimeout(() => {
 let ss =  document.createElement("div")
   ss.id = "html_element"
   document.body.appendChild(ss)
-}, 2000);
+}, 100);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
